2578a003e2a8dfe17ba9c737f9583c7b180c5da5
rayhan <aburayhandh@gmail.com>
2023-09-24 20:09:57 +0600

    Succesfully set up project to start and set up router,folder structure and Navbar

---
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+import Logo from "../../../assets/Logo.png";
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="max-w-[1024px] mx-auto flex justify-between items-center h-14 py-10">
+                <NavLink to='/' className='text-2xl font-semibold'>
+                    <img src={Logo} alt="" width={200} />
+                </NavLink>
+
+                <div className="flex justify-center items-center gap-9 text-lg font-medium">
+                    <NavLink to='/' className={({ isActive, isPending }) => isPending ? "text-black" : isActive ? "text-[#FF444A] border-b-2 border-[#FF444A]" : ""}>
+                        Home
+                    </NavLink>
+
+                    <NavLink to='/donation' className={({ isActive, isPending }) => isPending ? "text-black" : isActive ? "text-[#FF444A] border-b-2 border-[#FF444A]" : ""}>
+                        Donation
+                    </NavLink>
+
+                    <NavLink to='/statistics' className={({ isActive, isPending }) => isPending ? "text-black" : isActive ? "text-[#FF444A] border-b-2 border-[#FF444A]" : ""}>
+                        Statistics
+                    </NavLink>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
