78cd6a885924b6ab506d689b8dc17150af33bf39
rayhan <aburayhandh@gmail.com>
2023-09-25 21:45:22 +0600

    A responsive navbar added that navigate pages in small devices

--- src/Components/Header/Navbar/Navbar.jsx
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 import { NavLink } from "react-router-dom";
 import Logo from "../../../assets/Logo.png";
+import { useState } from "react";
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from "react-icons/ai";
+
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
         <div>
             <nav className="max-w-[1024px] mx-auto px-6 flex justify-between items-center h-14 py-10">
@@ -22,8 +27,28 @@ const Navbar = () => {
                         Statistics
                     </NavLink>
                 </div>
-            </nav>
-        </div>
+
+                <div onClick={() => setToggleMenu(!toggleMenu)} className="lg:hidden flex items-center cursor-pointer text-2xl">
+                    {
+                        toggleMenu === true ? <FaBars /> : <AiOutlineClose />
+                    }
+
+                    <div className={`lg:hidden flex flex-col items-center absolute top-16 z-40 right-11 gap-12 text-lg font-medium bg-white border-2 shadow-md px-20 py-10 rounded-xl ${toggleMenu ? 'hidden' : ''}`}>
+                        <NavLink to='/' className={({ isActive, isPending }) => isPending ? "text-black" : isActive ? "text-[#FF444A] border-b-2 border-[#FF444A]" : ""}>
+                            Home
+                        </NavLink>
+
+                        <NavLink to='/donation' className={({ isActive, isPending }) => isPending ? "text-black" : isActive ? "text-[#FF444A] border-b-2 border-[#FF444A]" : ""}>
+                            Donation
+                        </NavLink>
+
+                        <NavLink to='/statistics' className={({ isActive, isPending }) => isPending ? "text-black" : isActive ? "text-[#FF444A] border-b-2 border-[#FF444A]" : ""}>
+                            Statistics
+                        </NavLink>
+                    </div>
+                </div>
+            </nav >
+        </div >
     );
 };
 
